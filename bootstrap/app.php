dfb05517ff7934401c7b4fb3ad698a8c1ef29120
Mauko <maukoese@gmail.com>
2026-04-16 06:09:50 +0300

    Switch to nixpacks setup over Dockerfile

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,10 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+
         $middleware->validateCsrfTokens(except: [
             'embed/*',
             'livewire/messages/embed',
         ]);
+        
         $middleware->web(replace: [
             ValidateCsrfToken::class => CustomValidateCsrfToken::class,
         ]);
